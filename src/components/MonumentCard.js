5575d352852b660d68ed151e033666c3536b2178
KXRXH <kxrxh.contact@gmail.com>
2022-10-02 18:52:25 +0300

    README.md is updated!

--- src/components/MonumentCard.js
+++ src/components/MonumentCard.js
@@ -8,24 +8,27 @@ export default function MonumentCard({monumentInfo, state, changeState}) {
             open={state}
             onClose={() => changeState(false)}
             fullWidth={true}
-            maxWidth="lg"
-            sx={{backgroundColor: 'transparent'}}>
-            <Box sx={{display: 'flex'}}>
-                <DialogTitle>{monumentInfo['title']}</DialogTitle>
-                <IconButton
-                    sx={{marginLeft: 'auto', marginRight: '5px', color: highlight}}
-                    size="large"
-                    edge='end'
-                    onClick={() => changeState(false)}
-                    variant="contained"><CloseIcon/>
-                </IconButton>
+            maxWidth="md"
+            sx={{backgroundColor: 'transparent', maxWidth: '100%',}}>
+            <Box sx={{overflowX: 'hidden'}}>
+                <Box sx={{display: 'flex'}}>
+                    <DialogTitle sx={{maxWidth: '70%'}}>{monumentInfo['title']}</DialogTitle>
+                    <IconButton
+                        sx={{marginLeft: 'auto', marginRight: '5px', color: highlight}}
+                        size="large"
+                        edge='end'
+                        onClick={() => changeState(false)}
+                        variant="contained"><CloseIcon/>
+                    </IconButton>
+                </Box>
+                <DialogContent sx={{display: 'flow', maxWidth: '100%'}}>
+                    <img src={monumentInfo['img']} alt={'img'}
+                         style={{width: '40%', height: '30%', textAlign: 'center'}}/>
+                    <DialogContentText sx={{marginTop: '100px'}}>
+                        {monumentInfo['info']}
+                    </DialogContentText>
+                </DialogContent>
             </Box>
-            <DialogContent sx={{display: 'flow',}}>
-                <img src={monumentInfo['img']} alt={'img'} style={{width: '60%', height: '40%', textAlign: 'center'}}/>
-                <DialogContentText sx={{marginTop: '100px'}}>
-                    {monumentInfo['info']}
-                </DialogContentText>
-            </DialogContent>
         </Dialog>
     );
 }
